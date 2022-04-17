cdf2dff27ec86fcdfb62adad3f895d6a29dfcee6
r-big-island <lalaknockrara@gmail.com>
2022-04-17 12:17:13 +0900

    install ts-node-dev

--- src/install-typescript.ts
+++ src/install-typescript.ts
@@ -14,6 +14,15 @@ console.log({ message });
 // ▶︎ nodeコマンドで生成したjsファイルが実行されれば、実行環境の構築を正常に完了している
 
 // 🌟実際には、実行するツールを使って、開発に便利な状態を作ることを目指す！！
-// ▶︎ ts-nodeコマンドでコンパイル〜実行を一気に行う
+// ▶︎ ts-nodeコマンドでコンパイル〜実行を一気に行う: バージョン確認は npm info ts-node で確認
 //   npm install --save-dev ts-node@0.0.0 でts-nodeコマンドを実行できるようになる
 // ▶︎ 「npx ts-node [コンパイル〜実行したいtsファイル]」で出力結果が出れば完了
+
+// 🌟ts-nodeより便利に実行するツール
+// ▶︎ ts-node-devコマンド: 実行後にファイルの変更を監視し続け、変更を検知すると即座にコンパイル〜実行する
+//   ※インストール方法は「ts-node」と同様（コマンド名だけきちんと変えること）
+// ▶︎ npx ts-node-dev --respawn [実行したいファイル]: --respawnオプションでコンパイル〜実行のみを行ってくれる
+// ※package.jsonの"scripts"要素内に「npm run」で「ts-node-dev --respawn」を指定して実行可能！！
+//  "scripts"配下に「"dev": "ts-node-dev --respawn",」の様に持たせることで
+//  devがキーになり、「npm run dev [実行したいファイル]」＝「npx ts-node-dev --respawn [実行したいファイル]」と同意になる
+
